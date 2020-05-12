c9462bd463ac0e054b8f0cd999e9bcad205f9df5
miriwo0517 <shunokawa@gmail.com>
2020-05-12 23:48:42 +0900

    ユーザホームにボタンを設置した

--- resources/views/user_home/index.blade.php
+++ resources/views/user_home/index.blade.php
@@ -3,7 +3,14 @@
 @section('content')
     {{-- main start --}}
     <div class="main">
-        <p>user_home</p>
+        <div class="link">
+            <div class="content_button">
+                <input type="button" onclick="location.href=''" value="todo">
+            </div>
+            <div class="settings">
+                <input type="button" onclick="location.href=''" value="設定">
+            </div>
+        </div>
     </div>
     {{-- main end --}}
 @endsection
