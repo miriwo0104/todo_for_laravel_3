db69cdd4869c4942577fdfaa66e4cb8001297979
miriwo0517 <shunokawa@gmail.com>
2020-05-14 00:00:16 +0900

    todoを登録する処理を追加した

--- app/Http/Controllers/ContentController.php
+++ app/Http/Controllers/ContentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\TodoContent;
 
@@ -22,5 +23,18 @@ class ContentController extends Controller
 
     public function create(Request $request) {
         //受け取った値をDBに保存する処理を記載
+
+        $user_id = Auth::id();
+
+        $new_todo_info = new TodoContent();
+
+        $new_todo_info->title = $request['title'];
+        $new_todo_info->user_id = $user_id;
+        $new_todo_info->memo = $request['memo'];
+
+        $new_todo_info->save();
+
+        return redirect('/content/index');
+
     }
 }
